db5bf5da0e4598efa9714a397822decfadb1f6a8
shbm-rawat <shbm176140@gmail.com>
2026-06-06 15:56:39 +0530

    fix

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/todoSlice";
-import themeReducer from "../features/themeSlice";
 import authReducer from "../features/authSlice"
 const store = configureStore({
     reducer: {
         todo: todoReducer,
-        theme: themeReducer,
         auth: authReducer,
     }
 });
